270c080f38b9f86d11c52518f9ba7e1754f32241
Jester <jester.sebastiansantos@gmail.com>
2026-07-26 04:31:57 -0500

    feat: set basic page layout

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import "./styles/fw.css";
 import "./styles/reset.css";
 import "./styles/style.css";
 
-document.body.appendChild(document.createTextNode("hello world"));
+function main() {}
+
+main();
