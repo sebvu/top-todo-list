3d945f1966b96ab124d40de2cc1037a68a1811a5
Jester <jester.sebastiansantos@gmail.com>
2026-08-01 10:59:08 -0500

    feat: add basic classes

--- src/exporter.js
+++ src/exporter.js
@@ -1,1 +1,4 @@
 export { default as Loader } from "./helpers/elLoader.js";
+export { default as UIController } from "./controllers/UIController.js";
+export { default as StorageController } from "./controllers/StorageController.js";
+export { default as LogController } from "./controllers/LogController.js";
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import * as _ from "./style-exporter.js";
+import { UIController, StorageController } from "./exporter.js";
 
-function main() {}
+class TodoList {
+  constructor() {}
+}
+
+function main() {
+  const todoList = new TodoList();
+}
 
 main();
