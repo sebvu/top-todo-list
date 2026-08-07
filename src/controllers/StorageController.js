7108ed8c2c651e3d593dabf2f0b59f7f5fd1be07
Jester <jester.sebastiansantos@gmail.com>
2026-08-06 23:20:27 -0500

    feat: get and set current data functions

--- src/controllers/StorageController.js
+++ src/controllers/StorageController.js
@@ -1,4 +1,5 @@
 import LogController from "./LogController.js";
+import ProjectController from "./ProjectController.js";
 
 export default new (class StorageController {
   constructor() {}
@@ -23,81 +24,14 @@ export default new (class StorageController {
     return currSavedTheme;
   }
 
-  getSavedData() {
-    return [
-      {
-        projectName: "House Cleaning",
-        id: "wowowow",
-        todoLists: [
-          {
-            name: "general stuff to do",
-            listItems: [
-              {
-                name: "People to Invite",
-                dueDate: "2026-08-07",
-                description:
-                  "These people owe me. They OWE ME! I'VE HELPED THEM, THEY NEED TO HELP ME! HELP ME!!",
-                priorityLevel: "high",
-                checkList: [
-                  "Cristal",
-                  "Jester (me!?)",
-                  "Maya",
-                  "Latte",
-                  "Darelle",
-                  "Vanessa",
-                ],
-              },
-              {
-                name: "Stuff to clean",
-                dueDate: "2026-08-10",
-                description:
-                  "I hate doing this stuff.. Ugh. Lower priority cause I hate doing stuff",
-                priorityLevel: "medium",
-                checkList: [
-                  "floors",
-                  "walls",
-                  "bed",
-                  "the entire neighborhood (wtf)",
-                ],
-              },
-              {
-                name: "food",
-                dueDate: "2026-08-07",
-                description: "Need to get snacks.. ig :(",
-                priorityLevel: "medium",
-                checkList: ["cheetos"],
-              },
-              {
-                name: "cry",
-                dueDate: "2026-08-07",
-                description: "wanna cry",
-                priorityLevel: "low",
-                checkList: [],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        projectName: "the odin project",
-        id: "sdlfjksdjlk",
-        todoLists: [
-          {
-            name: "todo list",
-            listItems: [
-              {
-                name: "backend",
-                dueDate: "2027-08-07",
-                description: "work on the damn backend",
-                priorityLevel: "high",
-                checkList: ["backend stuff #1", "backend stuff #2"],
-              },
-            ],
-          },
-        ],
-      },
-    ];
+  saveCurrentData() {
+    localStorage.setItem(
+      this.#DATA_NAME,
+      JSON.stringify(ProjectController.getStructureJSON()),
+    );
   }
 
-  addProject() {}
+  getCurrentData() {
+    return JSON.parse(localStorage.getItem(this.#DATA_NAME));
+  }
 })();
